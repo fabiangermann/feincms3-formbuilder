7287778d02f2f63ebe5f69a02cead045a46a0c8f
Fabian Germann <fg@feinheit.ch>
2026-04-20 15:35:37 +0000

    Initial project scaffolding with test Django project

---
+++ tests/testapp/settings.py
@@ -0,0 +1,49 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = "test-secret-key-not-for-production"
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "content_editor",
+    "feincms3",
+    "feincms3_forms",
+    "testapp",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+            ],
+        },
+    },
+]
+
+ROOT_URLCONF = "testapp.urls"
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
